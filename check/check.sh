942eb22820ca79c92b9f1d2f9e37cc8c4236a9d9
Joseph Guillaume <josephguillaume@gmail.com>
2021-12-11 23:29:27 +0000

    patch oidc-provider

--- check/check.sh
+++ check/check.sh
@@ -15,3 +15,13 @@ check identity_handler.json.diff
 
 diff ../node_modules/@solid/community-server/config/util/resource-locker/memory.json ../config/memory.json >  memory.json.diff
 check memory.json.diff
+
+# Patches to be applied
+
+if cmp -s ../node_modules/oidc-provider/lib/actions/grants/refresh_token.js refresh_token.js; then
+	echo refresh token patch already applied
+else
+	diff -u ../node_modules/oidc-provider/lib/actions/grants/refresh_token.js refresh_token.js > refresh_token.js.diff
+	check refresh_token.js.diff
+	patch ../node_modules/oidc-provider/lib/actions/grants/refresh_token.js refresh_token.js.diff
+fi
